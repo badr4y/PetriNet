4ab601077ab9130b16f5099dd6e05fd4e37cc408
xyt <xyt3523@outlook.com>
2023-10-11 16:15:24 +0200

    transition 1

--- src/main/java/org/example/Transition.java
+++ src/main/java/org/example/Transition.java
@@ -1,4 +1,22 @@
 package org.example;
 
 public class Transition {
+	private ArrayList<Arc> arcs_entrant;
+	private ArrayList<Arc> arcs_sortant;
+	
+	public Transition() {
+		
+	}
+	public Transition(ArrayList<Arc> arcs_entrant, ArrayList<Arc> arcs_sortant) {
+		 arcs_entrant = arcs_entrant;
+	     arcs_sortant = arcs_sortant;
+	}
+	
+	public setArcs_entrants(ArrayList<Arc> arcs_entrants) {
+		arcs_entrant = arcs_entrants;
+	}
+	
+	public setArcs_sortant(ArrayList<Arc> arcs_sortrants) {
+		arcs_sortant = arcs_sortants;
+	}
 }
