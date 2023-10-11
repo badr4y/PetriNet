931211c8a52f9d17cf933b49f469598d1e596ae0
xyt <xyt3523@outlook.com>
2023-10-11 15:23:52 +0200

    modify the class arc_zero

--- src/main/java/org/example/Arc_zéro.java
+++ src/main/java/org/example/Arc_zéro.java
@@ -1,4 +1,15 @@
 package org.example;
 
-public class Arc_zéro {
+public class Arc_zéro extends Arc{
+	
+	public Arc_zéro(Place place, Transition transition) {
+		this.place=place;
+		this.transition=transition;
+	}
+	
+	@Override
+	public boolean Tirable() {
+		return place.getNb_Jetons() ==0;
+	}
+	
 }
