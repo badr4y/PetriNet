91734117ec54a47d3b502af696c0381401d3b523
xyt <xyt3523@outlook.com>
2023-10-11 14:39:30 +0200

    modify the Place class

--- src/main/java/org/example/Place.java
+++ src/main/java/org/example/Place.java
@@ -1,4 +1,19 @@
 package org.example;
 
 public class Place {
+	private int nb_jetons;
+	
+	
+	public Place(int n) {
+		nb_jetons = n;
+	}
+	public void Modifier_jetons(int m) {
+		nb_jetons = m;
+	}
+	public void getNb_Jetons() {
+		return nb_jetons;
+	}
+	public void setNb_Jetons(int m) {
+		nb_jetons = m;
+	}
 }
