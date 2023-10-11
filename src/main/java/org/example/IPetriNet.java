6f7af56e42f13d3b94a5c3ebf70e965553ac172f
badr4y <badrfourane@gmail.com>
2023-10-11 15:34:04 +0100

    implementation de PetriNet et son interface

--- src/main/java/org/example/IPetriNet.java
+++ src/main/java/org/example/IPetriNet.java
@@ -1,4 +1,15 @@
 package org.example;
 
 public interface IPetriNet {
+    void Ajouter_arc(Arc arc);
+    void Ajouter_place(Place place);
+    void Ajouter_transition(Transition transition);
+
+    void Supprimer_arc(Arc arc);
+    void Supprimer_transition(Transition transition);
+    void Supprimer_place(Place place);
+
+    void Fire(Transition transition);
+
+
 }
--- src/main/java/org/example/PetriNet.java
+++ src/main/java/org/example/PetriNet.java
@@ -1,7 +1,50 @@
 package org.example;
 
-public class PetriNet {
+import java.util.ArrayList;
+import java.util.List;
+
+public class PetriNet implements IPetriNet{
+
+    List<Arc> Arcs = new ArrayList<>();
+    List<Place> Places = new ArrayList<>();
+    List<Transition> Transitions = new ArrayList<>();
+
+    @Override
+    public void Ajouter_arc(Arc arc) {
+        Arcs.add(arc);
+    }
+
+    @Override
+    public void Ajouter_place(Place place) {
+        Places.add(place);
+    }
+
+    @Override
+    public void Ajouter_transition(Transition transition) {
+        Transitions.add(transition);
+    }
+
+    @Override
+    public void Supprimer_arc(Arc arc) {
+        Arcs.remove(arc);
+    }
+
+    @Override
+    public void Supprimer_transition(Transition transition) {
+        Transitions.remove(transition);
+    }
+
+    @Override
+    public void Supprimer_place(Place place) {
+        Places.remove(place);
+    }
+
+    @Override
+    public void Fire(Transition transition) {
+        transition.Activer();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
