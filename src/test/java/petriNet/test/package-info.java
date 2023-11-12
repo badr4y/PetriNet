f5eac86125457a95baa2bb5dc17f2ce347b31de2
xyt <xyt3523@outlook.com>
2023-11-12 16:19:50 +0100

    add some information of README.md modify a package name to avoid problems in tests

--- src/test/java/petriNet/test/package-info.java
+++ src/test/java/petriNet/test/package-info.java
@@ -1,10 +1,10 @@
 /**
  * This packages contains all unit tests for the Petri Network implementation {@link petrinet.main}
- * {@link petrinet.test.ArcTest} : contains unit tests for {@link petrinet.main.Arc}
- * {@link petrinet.test.ArcVideurTest} : contains unit tests for {@link petrinet.main.ArcVideur}
- * {@link petrinet.test.ArcZeroTest} : contains unit tests for {@link petrinet.main.ArcZero}
- * {@link petrinet.test.PetriNetTest} : contains unit tests for {@link petrinet.main.PetriNet}
- * {@link petrinet.test.PlaceTest} : contains unit tests for {@link petrinet.main.Place}
- * {@link petrinet.test.TransitionTest} : contains unit tests for {@link petrinet.main.Transition}
+ * {@link petriNet.test.ArcTest} : contains unit tests for {@link petrinet.main.Arc}
+ * {@link petriNet.test.ArcVideurTest} : contains unit tests for {@link petrinet.main.ArcVideur}
+ * {@link petriNet.test.ArcZeroTest} : contains unit tests for {@link petrinet.main.ArcZero}
+ * {@link petriNet.test.PetriNetTest} : contains unit tests for {@link petrinet.main.PetriNet}
+ * {@link petriNet.test.PlaceTest} : contains unit tests for {@link petrinet.main.Place}
+ * {@link petriNet.test.TransitionTest} : contains unit tests for {@link petrinet.main.Transition}
  */
-package petrinet.test;
+package petriNet.test;
